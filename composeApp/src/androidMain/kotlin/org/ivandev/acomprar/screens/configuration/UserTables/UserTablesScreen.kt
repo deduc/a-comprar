c3ad34d3146a18cf8c7c83a67e0936935b7a9251
PORTATIL-IVAN\deduc <igomez@edgeexpertise.es>
2025-10-02 12:20:19 +0200

    comenzar a crear usertables screen

---
+++ composeApp/src/androidMain/kotlin/org/ivandev/acomprar/screens/configuration/UserTables/UserTablesScreen.kt
@@ -0,0 +1,33 @@
+package org.ivandev.acomprar.screens.configuration.UserTables
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import cafe.adriel.voyager.core.screen.Screen
+import org.ivandev.acomprar.Literals
+import org.ivandev.acomprar.components.CommonScreen
+
+object UserTablesScreen: Screen {
+    @Composable
+    override fun Content() {
+        val screen = CommonScreen(
+            title = Literals.USER_TABLES_TITLE
+        ) {
+            MainContent()
+        }
+
+        screen.Render()
+    }
+
+    @Composable
+    fun MainContent() {
+        val userTables: List<String> = Literals.Database.USER_TABLES
+        Column {
+            userTables.forEach { table ->
+                Text(table)
+            }
+
+        }
+    }
+
+}
